02697f01f2e699d6ac02e294b3ea34fc048f7fe8
Unknown <jyeon86@gmail.com>
2018-05-28 15:02:37 +0900

    1일차: while, for, do-while문

---
+++ javascript/day1/js/while.js
@@ -0,0 +1,22 @@
+
+
+
+var i = 1;
+var sum = 0;
+while(i <=10){
+    sum+= i;    
+    i++;
+}
+console.log("합계 : "+sum);
+
+var sum1 = 0;
+for(i=1; i<=10; i++){
+    sum1 += i; 
+}
+console.log("sum : "+ sum);    
+/*
+i=1;
+do{ 
+    console.log("i : "+i);
+    i++;
+}while(i <=10)*/
