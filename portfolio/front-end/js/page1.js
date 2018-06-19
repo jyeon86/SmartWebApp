fdc9eb091b805fb2225dc9422f27fa76bfb638d5
Unknown <jyeon86@gmail.com>
2018-06-19 17:20:37 +0900

    portpolio2

--- portfolio/front-end/js/page1.js
+++ portfolio/front-end/js/page1.js
@@ -1,10 +1,18 @@
-
-
 $("#hamburger").click(function() {
-	$("#sidenav").css("right", "0");
-  $("body").addClass("dark")
+  $("#sidenav").css("right", "0");
+  //$("body").addClass("dark")
+  $("body").css("background","rgba(0,0,0,0.4)");
+  $("img").css("opacity","0.5");
+  $("iframe").css("opacity","0.5");
+  
 });
 $("#close-sidenav").click(function() {
-	$("#sidenav").css("right", "-200px");
-  $("body").removeClass("dark");
+	$("#sidenav").css("right", "-260px");
+  //$("body").removeClass("dark");
+  $("body").css("background","none");
+  $("img").css("opacity","1");
+  $("iframe").css("opacity","1");
+});
+$('.carousel').carousel({
+  interval: 2000
 });
